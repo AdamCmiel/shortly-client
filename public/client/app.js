9bc570124b178064be5069bd6c17ec45f3764b9b
AdamCmiel <adamcmiel@gmail.com>
2014-02-07 11:49:48 -0800

    set up data binding with controllers and models

--- public/client/app.js
+++ public/client/app.js
@@ -1,1 +1,9 @@
 //Rewrite angular app
+var app = angular.module("ShortlyApp", []);
+
+app.run(function($rootScope){
+  $rootScope.name = 'adam';
+});
+app.controller('nameController', function($scope){
+  $scope.name = '';
+});
